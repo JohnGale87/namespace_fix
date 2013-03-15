3f08d88481e0f8e8954d75908cf24b689b37fb39
John Gale <john.gale@appogee.co.uk>
2013-03-15 10:00:12 +0000

    initial commit

---
+++ AppEngineNamespaceFix.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        AppEngineNamespaceFix
+// @description Userscript fix for AppEngine Namespace Bug. http://code.google.com/p/googleappengine/issues/detail?id=8164
+// @namespace   http://127.0.0.1
+// @version     3
+// @include     http://www.appengine.google.com/*
+// @include     https://www.appengine.google.com/*
+// @include     http://appengine.google.com/*
+// @include     https://appengine.google.com/*
+// @match       http://www.appengine.google.com/*
+// @match       https://www.appengine.google.com/*
+// @match       http://appengine.google.com/*
+// @match       https://appengine.google.com/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// ==/UserScript==
+if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
+  this.GM_getValue=function (key,def) { return localStorage[key] || def; };
+  this.GM_setValue=function (key,value) { return localStorage[key]=value; };
+  this.GM_deleteValue=function (key) { return delete localStorage[key]; };
+  this.GM_listValues=function () { return localStorage; };
+};
+
+var url = window.location.href;
+if (url.indexOf('datastore/explorer') > -1 || url.indexOf('datastore/edit') > -1){
+	var namespace = GM_getValue("appEngine_Namespace", "-global-");
+	var namespace_set = (GM_getValue("appEngine_NamespaceSet", 0) > 0);
+	if (url.indexOf('&namespace=') < 0){
+		if (!namespace_set) namespace = prompt("Enter namespace...", namespace);
+		if (namespace != null){
+			GM_setValue("appEngine_Namespace", namespace);
+			GM_setValue("appEngine_NamespaceSet", 1);
+			if (namespace != null && namespace != ""){
+				window.location.href = url + '&namespace=' + namespace;
+			}
+		}
+	}
+} else {
+	if (GM_listValues().length > 0){
+		GM_setValue("appEngine_NamespaceSet", 0);
+		GM_deleteValue("appEngine_Namespace");
+		alert('AppEngineNamespaceFix: Namespace cleared.');
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,29 @@ namespace_fix
 =============
 
 AppEngine Datastore Namespace Fix
+
+So.... I finally got annoyed enough with this to bother doing something...
+
+Attached is my Chrome and Firefox extension that gets arround this annoying bug.
+
+For Chrome:
+    Save the file to your machine
+    Open Chrome > Tools > Extensions
+    Drag and drop
+
+For firefox:
+    Install greasemonkey add-on
+    Create a new user script
+    Copy and paste the contents of the attached into the new script.
+   
+Now whenever you go to the appengine datastore viewer you will be asked for a namespace. Once the namespace is entered you will not need to re-enter it again until you navigate away from the datastore viewer.
+To clear the saved namespace navigate away from the datastore viewer, just go to the dashboard or any other appengine page.
+
+The script only runs on appengine pages (those with appengine.google.com) in the url so will not interfear with anything else.
+
+Note: The script seems to run much faster in chrome than on firefox.
+
+
+Version 1: Initial Release.
+Version 2: Update to prevent the namespace being cleared when editing an entity.
+Version 3: Update to handle the -global- namespace much better than before.
